0b72222505f40cb19a8d189eb671e56a7bb2ee22
y-minion <dudals2963@tukorea.ac.kr>
2025-10-23 02:26:40 +0900

    각 테스트 후에 실행되야 할 로직을 afterEach 메서드를 통해 구현

--- __test__/userService.spec.ts
+++ __test__/userService.spec.ts
@@ -1,6 +1,10 @@
 import { users } from "src/data";
 import { UserService } from "src/utils/userService";
-import { expect, test } from "vitest";
+import { afterEach, expect, test } from "vitest";
+
+afterEach(() => {
+  users.splice(0);
+});
 
 test("findAll 테스트", () => {
   const userService = new UserService();
@@ -16,4 +20,12 @@ test("findAll 테스트", () => {
   expect(foundUsers).toHaveLength(3);
   expect(foundUsers).toContainEqual({ no: 1, email: "user1@gmail.com" });
   expect(foundUsers).toContainEqual({ no: 2, email: "user2@gmail.com" });
+});
+
+test("create()테스트", () => {
+  const userService = new UserService();
+  const user = { no: 4, email: "user4@test.com" };
+  userService.create(user);
+  expect(users).toHaveLength(1);
+  expect(users).toContainEqual(user);
 });
