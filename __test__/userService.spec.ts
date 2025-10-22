1ca2900fcc6f98eaa7984c1c9b3b81eb98b30be1
y-minion <dudals2963@tukorea.ac.kr>
2025-10-23 02:37:43 +0900

    각 test 전에 인스턴스를 만들고,user에 새로운 값들을 넣어주는 로직을 beforeEach통해 구현

--- __test__/userService.spec.ts
+++ __test__/userService.spec.ts
@@ -1,20 +1,24 @@
 import { users } from "src/data";
 import { UserService } from "src/utils/userService";
-import { afterEach, expect, test } from "vitest";
+import { afterEach, beforeEach, expect, test } from "vitest";
+
+let userService: UserService;
 
 afterEach(() => {
   users.splice(0);
 });
 
-test("findAll 테스트", () => {
-  const userService = new UserService();
+beforeEach(() => {
+  userService = new UserService();
 
   users.push(
     { no: 1, email: "user1@gmail.com" },
     { no: 2, email: "user2@gmail.com" },
     { no: 3, email: "user3@gmail.com" }
   );
+});
 
+test("findAll 테스트", () => {
   const foundUsers = userService.findAll();
 
   expect(foundUsers).toHaveLength(3);
@@ -23,9 +27,15 @@ test("findAll 테스트", () => {
 });
 
 test("create()테스트", () => {
-  const userService = new UserService();
   const user = { no: 4, email: "user4@test.com" };
   userService.create(user);
-  expect(users).toHaveLength(1);
+  expect(users).toHaveLength(4);
   expect(users).toContainEqual(user);
+});
+
+test("delete()", () => {
+  const no = 3;
+  userService.delete(no);
+  expect(users).toHaveLength(2);
+  expect(users).not.toContainEqual({ no: 3, email: "user3@test.com" });
 });
